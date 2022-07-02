4f789f83fa26b27f2fef551c01f2738e9a4fcea3
Willy Tekeu <willy.tekeu@gmail.com>
2022-07-02 16:03:42 -0400

    added package

---
+++ Sources/NetworkInterceptor/NetworkInterceptor.swift
@@ -0,0 +1,6 @@
+public struct NetworkInterceptor {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/NetworkInterceptorTests/NetworkInterceptorTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import NetworkInterceptor
+
+final class NetworkInterceptorTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(NetworkInterceptor().text, "Hello, World!")
+    }
+}
